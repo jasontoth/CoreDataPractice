25e036dc6e9ec48c6a1189314da4aa5d00466292
jasontoth <jtoth313@gmail.com>
2018-08-16 21:08:27 -0500

    - Add view controller for new task screen - Add fields and buttons to new task screen

---
+++ CoreDataPractice/CreateTaskViewController.swift
@@ -0,0 +1,31 @@
+//
+//  CreateTaskViewController.swift
+//  CoreDataPractice
+//
+//  Created by Jason Toth on 8/16/18.
+//  Copyright © 2018 Jason Toth. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    @IBOutlet weak var taskNameTextField: UITextField!
+    @IBOutlet weak var importantSwitch: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func saveTask(_ sender: Any) {
+    }
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
